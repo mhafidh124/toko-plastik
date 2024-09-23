a1a9972110ccc7e9d22e77f15d94a50b975b1f82
mhafidh124 <mhafidh124@gmail.com>
2024-09-23 12:12:02 +0700

    modified: index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-        <title>Data Barang</title>
+        <title>Data Barang BARANG</title>
     </head>
 
     <body>
